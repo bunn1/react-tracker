525ad093c4e2eb31fdaaf167f35b4ecedfe24812
bunn1 <nicjohbun@gmail.com>
2023-10-14 11:45:53 +0200

    App component - with functions and tasks objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 // Behövs när man jobbar med classes
 // import React from 'react';
-
+import { useState } from 'react'
 import Header from "./components/Header.js";
+import Tasks from "./components/Tasks.js";
 
 function App() {
+
+  const [ tasks, setTasks ] = useState( [
+    { 
+        id: 1, 
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30',
+        reminder: true,
+    },
+    { 
+
+        id: 2, 
+        text: 'Meeting at school',
+        day: 'Feb 6th at 4:30',
+        reminder: true,
+
+    },
+    { 
+        id: 3, 
+        text: 'Food shopping',
+        day: 'Feb 9th at 6:30',
+        reminder: false,
+
+    }
+]);
+
+// Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+
+// Toggle reminder
+const toggleReminder = (id) => {
+setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No task to show'}
     </div>
   );
 }
