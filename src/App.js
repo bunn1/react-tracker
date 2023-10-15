994f8ef067b7094669f99fa79aa0bd898a352e73
bunn1 <nicjohbun@gmail.com>
2023-10-15 08:17:56 +0200

    import Footer, About Fetch tasks and Add tasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // Behövs när man jobbar med classes
-// import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-
+import Footer from "./components/Footer";
+import About from "./components/About.js";
+  
 
 function App() {
 
@@ -12,39 +14,118 @@ function App() {
   const [ tasks, setTasks ] = useState( []);
 
   useEffect(() =>{
-    
-  })
+   const getTasks = async () => {
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+// Fetch tasks
+const fetchTasks = async () => {
+const res = await fetch('http://localhost:5000/tasks')
+const data = await res.json()
+
+return data
+}
+
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+
 
 // Add task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) +1
-  console.log(id);
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask]) 
+const addTask = async (task) => {
+
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+
+  // const id = Math.floor(Math.random() * 10000) +1
+  // console.log(id);
+  // const newTask = {id, ...task}
+  // setTasks([...tasks, newTask]) 
 }
 
 // Delete task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task) => task.id !== id))
+const deleteTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE',
+  })
+  //We should control the response status to decide if we will change the state or not.
+  res.status === 200
+    ? setTasks(tasks.filter((task) => task.id !== id))
+    : alert('Error Deleting This Task')
+}
+
+ // Toggle Reminder
+ const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(updTask),
+  })
+
+  const data = await res.json()
+
+  setTasks(
+    tasks.map((task) =>
+      task.id === id ? { ...task, reminder: data.reminder } : task
+    )
+  )
 }
 
 
-// Toggle reminder
-const toggleReminder = (id) => {
-setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
-}
-
-
-
-
-  return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} 
-      showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No task to show'}
+return (
+  <Router>
+    <div className='container'>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No Tasks To Show'
+              )}
+            </>
+          }
+        />
+        <Route path='/about' element={<About />} />
+      </Routes >
+      <Footer />
     </div>
-  );
+  </Router>
+)
 }
 
-export default App;
+export default App
